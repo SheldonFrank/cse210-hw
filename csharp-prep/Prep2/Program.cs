0ff5c583932c93203c50c3e77e12efbcfdde1efb
unknown <alistairgstewart01@gmail.com>
2024-09-20 18:13:18 -0600

    finished prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            
+            letter = "A";
+
+        }
+
+        else if (grade >= 80)
+        {
+            
+            letter = "B";
+
+        }
+
+        else if (grade >= 70)
+        {
+            
+            letter = "C";
+
+        }
+
+        else if (grade >= 60)
+        {
+            
+            letter = "D";
+
+        }
+
+        else
+        {
+            
+            letter = "F";
+
+
+        }
+
+        Console.WriteLine(letter);
+
+        if (grade >= 70)
+        {
+            
+            Console.WriteLine("Congrats, you've passed the course!");
+
+        }
+
+        else
+        {
+
+            Console.WriteLine("You've failed the course. Better luck next time.");
+
+        }
+
+
+        
+
     }
 }
