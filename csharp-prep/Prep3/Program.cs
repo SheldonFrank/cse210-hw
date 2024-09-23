3d4979921962eb7fd5faf74091e40b7ce6de89a4
unknown <alistairgstewart01@gmail.com>
2024-09-23 14:52:42 -0600

    completed prep_3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random rnd = new Random();
+        int magic_number = rnd.Next(1, 100);
+        int guess = -1;
+        int count = 0;
+
+        while (guess != magic_number)
+    {
+        Console.WriteLine("Guess an integer between 1 and 100");
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess < magic_number)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (guess > magic_number)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        count++;
+
+        
     }
-}
+
+            Console.WriteLine($"You guessed it with {count} tries");
+}}
