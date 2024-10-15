de410f7f35352b6dcaca878052d741ccf06f0a9c
unknown <alistairgstewart01@gmail.com>
2024-10-15 14:47:23 -0600

    finished encapsulation activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        List<Fraction> fractions = [new Fraction(), new Fraction(5), new Fraction(3, 4)];
+
+        foreach (Fraction f in fractions) {
+
+            Console.WriteLine(f.GetFractionString());
+            Console.WriteLine(f.GetDecimalValue());
+
+        }
+
+
+        
     }
 }
---
+++ sandbox/CashRegister/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+
+    {
+
+    Console.WriteLine("Hello, Cash Register!");
+
+
+    Bin myBin = new("quarters", 40, (float)0.25);
+    myBin.ModifyAmount(6);
+    Console.WriteLine(myBin.TotalValue());
+
+
+    }
+
+}
+
+
